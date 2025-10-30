f2ceef651ddb2070fbad353c9858c66ec937134d
XandGVaz <vitorgarciavaz@gmail.com>
2025-10-30 11:30:17 -0300

    feature: classe para controle do servo

--- servo_control/include/servo.hpp
+++ servo_control/include/servo.hpp
@@ -10,19 +10,24 @@
 #define SERVO_PWM_FREQ 50           // hertz
 #define SERVO_PWM_PERIOD 20000      // microssegunds
 #define SERVO_PWM_BITS 13           // 0 - 8191
-#define SERVO_PWM_TIMER_CHANNEL 0   // canal 0 de timer   
+
+/*===============================================================================*/
+// Operação matemática angle -> duty cicle
+#define angleToDutyCicle(angle, servoPwmBits, servoPwmPeriod) \
+  (angle / 180.0)*(2500.0/servoPwmPeriod)*(1U << servoPwmBits)
 
 /*===============================================================================*/
 // Classe do servo
 class Servo{
 
   uint8_t servoPin;
+  uint8_t pwmChannel;
  
  public:
 
-  Servo(uint8_t servoPin);
-  void begin();
-  void setAngle(uint8_t angle);
+  Servo(uint8_t servoPin, uint8_t pwmChannel);
+  bool setup();
+  bool setAngle(uint8_t angle);
 };
 
 #endif
